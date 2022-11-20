29921605007a625d2c15a778bcd8c75297e0e772
digitaltvguy <chris.seeger@nbcuni.com>
2022-11-20 18:08:58 -0500

    Text Window, no storage

---
+++ AdvMediaLabVideoPlayerMobile/JSON_WIndow_ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  JSON_WIndow_ViewController.swift
+//  AdvMediaLab Video Player
+//
+//  Created by Chris Seeger on 11/20/22.
+//  Copyright © 2022 NBCUniversal. All rights reserved.
+//
+
+import UIKit
+
+class JSON_WIndow_ViewController: UIViewController {
+
+    
+     @IBAction func close() {
+         dismiss(animated: true,completion: nil)
+     }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
